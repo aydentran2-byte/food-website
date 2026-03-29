f27e59938140f0ef2d7dcefe4c556f7d39c7736c
aydentran2-byte <ayden.tran2@education.nsw.gov.au>
2026-03-30 02:02:09 +1100

    Create routes.py

---
+++ users/users/recipes/routes.py
@@ -0,0 +1,47 @@
+from flask import Blueprint, render_template, url_for, flash, redirect, request, abort
+from flask_login import current_user, login_required
+from flaskblog import db, limiter
+from flaskblog.models import Post
+from flaskblog.recipes.forms import PostForm
+from markupsafe import escape
+
+posts = Blueprint('posts', __name__)
+
+@posts.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        # Use escape to prevent XSS
+        post = Post(
+            title=escape(form.title.data),
+            content=form.content.data,
+            author=current_user
+        )
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('main.home'))
+    return render_template('create_post.html', title='New Post', form=form)
+
+@posts.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+@posts.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    
+    # Ensure only post author can update
+    if post.author != current_user:
+        abort(403)
+    
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = escape(form.title.data)
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('posts.post', post_id=post.i
